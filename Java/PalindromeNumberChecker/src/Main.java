85324d9f1a7526732e8831397090e139f80e32b0
yhertekin <yhertekin@gmail.com>
2023-09-04 23:06:07 +0300

    palindrome checker

---
+++ Java/PalindromeNumberChecker/src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        if (isPalindrome(number)) {
+            System.out.println(number + " is a palindrome.");
+        } else {
+            System.out.println(number + " is not a palindrome.");
+        }
+    }
+
+    public static boolean isPalindrome(int num) {
+        int originalNum = num;
+        int reversedNum = 0;
+
+        while (num > 0) {
+            int lastDigit = num % 10;
+            reversedNum = reversedNum * 10 + lastDigit;
+            num /= 10;
+        }
+
+        return originalNum == reversedNum;
+    }
+}
