b3369e3c4f1aa0efb4a1e6fd944eef5bdcb91adf
yhertekin <yhertekin@gmail.com>
2023-09-16 20:18:13 +0300

    add average in array

---
+++ Java/AverageArray/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] numbers = {1,2,3,4,5};
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+
+        System.out.println(sum / numbers.length);
+    }
+}
